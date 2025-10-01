529fa5e9f65e768f4bcb09127f890cc49153427d
S.hub|Hacker <sgc9850262470@gmail.com>
2025-10-01 15:41:41 +0530

    Removed previous code

--- main.go
+++ main.go
@@ -1,16 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net/http"
-
-	apis "github.com/Shubhacker/interview-2.0.git~/APIS"
-)
-
 func main() {
-	r := apis.APIS()
-	port := "8080"
-	fmt.Println("Server running at :", port)
-	log.Fatal(http.ListenAndServe(":"+port, r))
+
 }
